d88016452e4cb72cb366fc4fb01e1df168cc94a5
User <User@HT402-ST58>
2017-11-15 10:49:12 +0800

    201711151049

---
+++ MyJava/src/tw/brad/java/Brad01.java
@@ -0,0 +1,18 @@
+package tw.brad.java;
+
+import javax.swing.JOptionPane;
+
+public class Brad01 {
+	public static void main(String[] args) {
+		
+		String id = JOptionPane.showInputDialog("輸入....");
+		System.out.println(id);
+		System.out.println(id.length());
+		if (id.length() == 10){
+			
+		}else{
+			System.out.println("xxxxxx");
+		}
+		
+	}
+}
