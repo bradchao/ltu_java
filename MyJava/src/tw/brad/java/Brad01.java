5803acf2fb00e3e098d5dc908b497eee6610f309
User <User@HT402-ST58>
2017-11-15 11:51:49 +0800

    201711151151

--- MyJava/src/tw/brad/java/Brad01.java
+++ MyJava/src/tw/brad/java/Brad01.java
@@ -8,7 +8,18 @@ public class Brad01 {
 		String id = JOptionPane.showInputDialog("輸入....");
 		System.out.println(id);
 		if (id.matches("^[A-Z][12][0-9]{8}$")){
+//		if (id.matches("^09[0-9]{2}-[0-9]{6}$")){
 			System.out.println("OK");
+			
+			String letters = "ABCDEFGHJKLMNPQRSTUVXYWZIO";
+			char id1 = id.charAt(0);
+			int n12 = letters.indexOf(id1) + 10;
+			System.out.println(n12);
+			int n1 = n12 / 10;
+			int n2 = n12 % 10;
+			
+			
+			
 		}else{
 			System.out.println("XX");
 		}
